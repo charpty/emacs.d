640566cc43c39da7dd719a72f9ccc390023f5a24
qisa.cb <qisa.cb@cainiao.com>
2019-11-20 00:01:00 +0800

    remove some packages and conf optimize

--- init.el
+++ init.el
@@ -1,23 +1,23 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 
-(setq make-backup-files nil)
+(require 'package)
+
+;;; Code:
 (global-set-key (kbd "C-c C-c M-x") #'execute-extended-command)
 
 (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                           ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" default))
- '(package-selected-packages
-   '(modern-cpp-font-lock multiple-cursors smartparens irony-eldoc flycheck company irony ample-zen-theme clang-format helm-projectile projectile magit helm)))
+ '(custom-safe-themes '("2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" default))
+ '(helm-completion-style 'emacs)
+ '(package-selected-packages '(elisp-format modern-cpp-font-lock multiple-cursors smartparens
+                                            irony-eldoc company irony ample-zen-theme clang-format
+                                            helm-projectile projectile magit helm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,23 +25,22 @@
  ;; If there is more than one, they won't work right.
  )
 
+(menu-bar-mode -1)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (global-set-key (kbd "C-x b") #'helm-buffers-list)
-;(setq helm-ff-auto-update-initial-value nil)
-(menu-bar-mode -1)
-(with-eval-after-load 'helm
-  (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
-  (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
-  (define-key helm-map (kbd "C-z") #'helm-select-action))
+(with-eval-after-load 'helm (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+                      (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
+                      (define-key helm-map (kbd "C-z") #'helm-select-action))
 
 (helm-mode 1)
 
-
+;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-c t") 'ff-get-other-file)
 
+;; theme
 (load-theme 'ample-zen t)
 
 ;; irony-mode
@@ -55,29 +54,35 @@
 (add-hook 'c++-mode-hook 'company-mode)
 (add-hook 'c-mode-hook 'company-mode)
 
-;; flycheck-mode
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-(eval-after-load 'flycheck
-'(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-
 ;; eldoc-mode
 (add-hook 'irony-mode-hook 'irony-eldoc)
 
-(require 'smartparens-config)
-(add-hook 'c++-mode-hook 'smartparens-mode)
-(add-hook 'c-mode-hook 'smartparens-mode)
+;; flycheck
+(require 'flycheck)
+; Disable clang check, gcc check works better
+(setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(c/c++-clang)))
+; Enable C++11 support for gcc
+(add-hook 'c++-mode-hook (lambda ()
+                           (setq flycheck-gcc-language-standard "c++11")))
+(global-flycheck-mode)
 
+;; clang-format
 (require 'clang-format)
 (global-set-key [C-M-tab] 'clang-format-region)
 (fset 'c-indent-region 'clang-format-region)
 (global-set-key (kbd "C-c i") 'clang-format-region)
+(global-set-key (kbd "C-c u") 'clang-format-buffer)
 
 (setq default-tab-width 4)
 (setq-default indent-tabs-mode nil)
-(setq c-default-style "linux"
-      c-basic-offset 4)
-;((multiple-cursors-detect) (kbd "C-c u") 'clang-format-buffer)
+(setq c-default-style "linux" c-basic-offset 4)
 
 (require 'modern-cpp-font-lock)
 (modern-c++-font-lock-global-mode t)
+
+;; set this to prevent emacs from broken hard link
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+(provide 'init)
+;;; init ends here
